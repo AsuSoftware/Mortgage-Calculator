aaa4ac09fb256f7d76ba15c82959bc97e83caed0
AsuSoftware <52882447+AsuSoftware@users.noreply.github.com>
2020-04-07 19:34:43 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,25 +9,57 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         // Principal
-        System.out.println("Principal : ");
-        String principalFromUser =  scanner.nextLine().trim();
-        double principal = Double.parseDouble(principalFromUser);
 
-        // Annual rate
-        System.out.println("Annual interest rate : ");
-        String annualRateFromUser = scanner.nextLine().trim();
-        double annualRate = (Double.parseDouble(annualRateFromUser) / 100) / 12;
+            float principal = 0;
+            double annualRate = 0;
 
-        // Period
-        System.out.println("Period (Years) : ");
-        String periodFromUser = scanner.nextLine().trim();
-        double period = Double.parseDouble(periodFromUser) * 12;
+            while(true) {
+                System.out.print("Principal ($1k - $1M) : ");
+                principal = Float.parseFloat(scanner.nextLine().trim());
 
-        // Total
-        double total = principal * ((annualRate * Math.pow((1 + annualRate), period)) / (Math.pow((1 + annualRate), period) - 1));
+                if (principal >= 1000 && principal <= 1_000_000) {
+                    break;
+                } else {
+                    System.out.println("Enter a number between 1,000 and 1,000,000");
+                    continue;
+                }
+            }
 
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        String result = currency.format(total);
-        System.out.println("Mortgage : " + result);
+            while(true) {
+
+                System.out.print("Annual interest rate : ");
+                annualRate = Double.parseDouble(scanner.nextLine().trim());
+
+                if (annualRate > 0 && annualRate <= 30) {
+                    annualRate = (annualRate / 100) / 12;
+                    break;
+                } else {
+                    System.out.println("Enter a value greater than 0 and less than or equal 30.");
+                    continue;
+                }
+            }
+
+            while (true) {
+
+                // Period
+                System.out.print("Period (Years) : ");
+                double period = Double.parseDouble(scanner.nextLine().trim());
+                if(period >= 1 && period <= 30) {
+
+                    period *= 12;
+                    // Total
+                    double total = principal * ((annualRate * Math.pow((1 + annualRate), period)) / (Math.pow((1 + annualRate), period) - 1));
+
+                    NumberFormat currency = NumberFormat.getCurrencyInstance();
+                    String result = currency.format(total);
+                    System.out.println("Mortgage : " + result);
+                    break;
+
+                } else {
+                    System.out.println("Enter a value between 1 and 30.");
+                    continue;
+                }
+            }
+
     }
 }
